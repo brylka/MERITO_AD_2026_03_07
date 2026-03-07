ca37e5be88e6b37feb8280df408f1e8b6285e6f9
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 13:33:19 +0100

    Sobota

--- 4.py
+++ 4.py
@@ -8,19 +8,18 @@ X, y = data.data, data.target
 model = DecisionTreeClassifier()
 model.fit(X, y)
 
-# print(data)
+#print(data)
+# [4.9, 3. , 1.4, 0.2]
+# [6.1, 3. , 4.6, 1.4]
+# [6.9, 3.1, 5.4, 2.1]
 
-sepal_length = 6
-sepal_width = 3
-petal_length = 4
-petal_width = 1.5
+while True:
+    sepal_length = float(input("Sepal lenght: "))
+    sepal_width =  float(input("Sepal width:  "))
+    petal_length = float(input("Petal lenght: "))
+    petal_width =  float(input("Petal width:  "))
 
-cechy = [[sepal_length, sepal_width, petal_length, petal_width]]
-y_pred = model.predict(cechy)[0]
+    cechy = [[sepal_length, sepal_width, petal_length, petal_width]]
+    y_pred = model.predict(cechy)[0]
 
-print(f"Wynik: {data.target_names[y_pred]}")
-
-
-
-
-
+    print(f"Wynik: {data.target_names[y_pred]}")
