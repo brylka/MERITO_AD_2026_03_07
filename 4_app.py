848925edcd05dc4e6c95609815268117ccdcf549
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 14:04:42 +0100

    Sobota

--- 4_app.py
+++ 4_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import joblib
 
 app = Flask(__name__)
@@ -17,8 +17,20 @@ klasy = ['setosa', 'versicolor', 'virginica']
 #     #print(f"Wynik: {data.target_names[y_pred]}")
 #     print(f"Wynik: {klasy[y_pred]}")
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        sepal_length = float(request.form['sepal_length'])
+        sepal_width = float(request.form['sepal_width'])
+        petal_length = float(request.form['petal_length'])
+        petal_width = float(request.form['petal_width'])
+
+        cechy = [[sepal_length, sepal_width, petal_length, petal_width]]
+        y_pred = model.predict(cechy)[0]
+        #
+        #     #print(f"Wynik: {data.target_names[y_pred]}")
+        print(f"Wynik: {klasy[y_pred]}")
+
     return render_template("index.html")
 
 if __name__ == "__main__":
