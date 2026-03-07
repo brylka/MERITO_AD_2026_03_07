34a56cc0b43810eb19c659329870150e362d88ae
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 10:01:42 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -12,7 +12,11 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                 test_size=0.3, random_state=42)
 
-dt_classifier = DecisionTreeClassifier(random_state=42)
+dt_classifier = DecisionTreeClassifier(
+    max_depth=None,
+    criterion='entropy',
+    min_samples_split=10,
+    random_state=42)
 
 dt_classifier.fit(X_train, y_train)
 
