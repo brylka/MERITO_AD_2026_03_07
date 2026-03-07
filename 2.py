a91ddd2ce1a31de580628aa8643efc8e6ba80d41
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 10:23:22 +0100

    Sobota

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 from sklearn import tree
 
@@ -10,13 +10,9 @@ X = iris.data
 y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                test_size=0.3, random_state=42)
+                                test_size=0.8, random_state=42)
 
-dt_classifier = DecisionTreeClassifier(
-    max_depth=None,
-    criterion='entropy', # 'gini'
-    min_samples_split=10,
-    random_state=42)
+dt_classifier = KNeighborsClassifier()
 
 dt_classifier.fit(X_train, y_train)
 
@@ -27,7 +23,4 @@ print("Raport klasyfikacji:")
 print(classification_report(y_test, y_pred, target_names=iris.target_names))
 
 
-plt.figure(figsize=(15,10))
-tree.plot_tree(dt_classifier, feature_names=iris.feature_names,
-               class_names=iris.target_names, filled=True)
-plt.show()
+
