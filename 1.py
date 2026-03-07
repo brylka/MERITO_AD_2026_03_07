d6efb71c589a5dd6a6e87fa65c717828ec4094ea
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 09:21:52 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -1,7 +1,17 @@
 from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from sklearn import tree
 
 iris = load_iris()
 X = iris.data
 y = iris.target
 
-print(y)
+dt_classifier = DecisionTreeClassifier(random_state=42)
+
+dt_classifier.fit(X, y)
+
+plt.figure(figsize=(15,10))
+tree.plot_tree(dt_classifier, feature_names=iris.feature_names,
+               class_names=iris.target_names, filled=True)
+plt.show()
