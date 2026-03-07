2b67b761e4ac16cd3dbe9af7d9c83e63bfc6bfd6
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 13:23:26 +0100

    Sobota

--- 4.py
+++ 4.py
@@ -6,10 +6,19 @@ data = load_iris()
 X, y = data.data, data.target
 
 model = DecisionTreeClassifier()
-
 model.fit(X, y)
 
-y_pred = model.predict(...)
+# print(data)
+
+sepal_length = 6
+sepal_width = 3
+petal_length = 4
+petal_width = 1.5
+
+cechy = [[sepal_length, sepal_width, petal_length, petal_width]]
+y_pred = model.predict(cechy)[0]
+
+print(f"Wynik: {data.target_names[y_pred]}")
 
 
 
