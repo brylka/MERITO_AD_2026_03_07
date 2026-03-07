836edea49b1cc6d0d44eb59774b07977e807bf49
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 09:37:22 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from sklearn import tree
@@ -7,9 +8,18 @@ iris = load_iris()
 X = iris.data
 y = iris.target
 
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                test_size=0.8, random_state=42)
+
 dt_classifier = DecisionTreeClassifier(random_state=42)
 
-dt_classifier.fit(X, y)
+dt_classifier.fit(X_train, y_train)
+
+y_pred = dt_classifier.predict(X_test)
+
+print(y_test)
+print(y_pred)
+
 
 plt.figure(figsize=(15,10))
 tree.plot_tree(dt_classifier, feature_names=iris.feature_names,
