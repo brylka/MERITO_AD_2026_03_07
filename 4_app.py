58be0c76ada5cb9ccd4f2e426dae67710038129e
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-07 13:42:53 +0100

    Sobota

---
+++ 4_app.py
@@ -0,0 +1,16 @@
+import joblib
+
+model = joblib.load('model.joblib')
+klasy = ['setosa', 'versicolor', 'virginica']
+
+while True:
+    sepal_length = float(input("Sepal lenght: "))
+    sepal_width =  float(input("Sepal width:  "))
+    petal_length = float(input("Petal lenght: "))
+    petal_width =  float(input("Petal width:  "))
+
+    cechy = [[sepal_length, sepal_width, petal_length, petal_width]]
+    y_pred = model.predict(cechy)[0]
+
+    #print(f"Wynik: {data.target_names[y_pred]}")
+    print(f"Wynik: {klasy[y_pred]}")
